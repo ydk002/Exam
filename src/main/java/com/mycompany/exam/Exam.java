7aef6871ae8c4d1e8b4abe7635048eb5ecf2343e
ydk002 <antalszabiii@gmail.com>
2025-11-03 09:05:38 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/exam/Exam.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exam;
+
+/**
+ *
+ * @author antal
+ */
+public class Exam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
